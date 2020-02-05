e66928a17d3fb6ed7365f1a95ae50cee46c99a09
lucasmartinoia <lucas.martinoia@gmail.com>
2020-02-04 21:20:18 -0300

    Focus on PrimaryAPI API+Websocket

--- Testing/PrimaryAPI.cs
+++ Testing/PrimaryAPI.cs
@@ -1,7 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.IO;
-using PrimaryFixTester;
 
 namespace Testing
 {
@@ -11,23 +10,6 @@ namespace Testing
         [TestMethod]
         public void Init()
         {
-            LogFiles log = new LogFiles();
-            QFApplication _qf;
-
-            if (!File.Exists("..\..\config\tradeclientbyma.cfg"))
-            {
-                Console.WriteLine("No existe el archivo de configuracion MD");
-            }
-            else
-            if (_qf != null)
-            {
-                _qf.RegisterFormController(log.WriteLog);
-                _qf.RegisterCboSecurityListData(UpdateCboSecurityList);
-                _qf.RegisterCboMarketData(UpdateMarketData);
-                _qf.RegisterDGV(UpdateFormDataDGV);
-
-                _qf.Initiate(txtRutaConfigMD.Text, txtUsuarioMD.Text, txtPasswordMD.Text, true, this);
-            }
         }
     }
 }
