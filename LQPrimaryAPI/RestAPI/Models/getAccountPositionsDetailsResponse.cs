b0068d78b09b97946084456fbb9889cae6a2a2e1
lucasmartinoia <lucas.martinoia@gmail.com>
2020-03-03 14:31:41 -0300

    Position details

--- LQPrimaryAPI/RestAPI/Models/getAccountPositionsDetailsResponse.cs
+++ LQPrimaryAPI/RestAPI/Models/getAccountPositionsDetailsResponse.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -81,6 +82,7 @@ namespace LatamQuants.PrimaryAPI.Models
             public string message { get; set; }
             public string description { get; set; }
             public DetailedPosition detailedPosition { get; set; }
+            public List<Position> colPositions { get; set; }
 
             public static RootObject Parse(string sJson)
             {
@@ -88,9 +90,73 @@ namespace LatamQuants.PrimaryAPI.Models
 
                 oReturn = JsonConvert.DeserializeObject<Models.getAccountPositionsDetailsResponse.RootObject>(sJson);
 
+                // Parse positions
+                JObject oJson = JObject.Parse(sJson);
+
+                // get JSON result objects into a list
+                IList<Position> colPositions = oJson["detailedPosition"]["report"].Children().ToList();
+
+                // serialize JSON results into .NET objects
+                IList<SearchResult> searchResults = new List<SearchResult>();
+                foreach (JToken result in results)
+                {
+                    // JToken.ToObject is a helper method that uses JsonSerializer internally
+                    SearchResult searchResult = result.ToObject<SearchResult>();
+                    searchResults.Add(searchResult);
+                }
 
                 return oReturn;
             }
+        }
+
+
+
+        public class Position
+        {
+            public string symbolReference { get; set; }
+            public double instrumentInitialSize { get; set; }
+            public double instrumentFilledSize { get; set; }
+            public double instrumentCurrentSize { get; set; }
+            public string contractType { get; set; }
+            public double priceConversionFactor { get; set; }
+            public double contractSize { get; set; }
+            public double marketPrice { get; set; }
+            public string currency { get; set; }
+            public double exchangeRate { get; set; }
+            public double contractMultiplier { get; set; }
+
+            public List<PositionDetails> colPositionDetails { get; set; }
+        }
+
+        public class PositionDetails
+        {
+            public double totalInitialSize { get; set; }
+            public double buyInitialSize { get; set; }
+            public double sellInitialSize { get; set; }
+            public double buyInitialPrice { get; set; }
+            public double sellInitialPrice { get; set; }
+            public double totalFilledSize { get; set; }
+            public double buyFilledSize { get; set; }
+            public double sellFilledSize { get; set; }
+            public double buyFilledPrice { get; set; }
+            public double sellFilledPrice { get; set; }
+            public double totalCurrentSize { get; set; }
+            public double buyCurrentSize { get; set; }
+            public double sellCurrentSize { get; set; }
+            public DailyDifference detailedDailyDiff { get; set; }
+        }
+
+        public class DailyDifference
+        {
+            // detailedDailyDiff
+            public double buyPricePPPDiff { get; set; }
+            public double sellPricePPPDiff { get; set; }
+            public double totalDailyDiff { get; set; }
+            public double buyDailyDiff { get; set; }
+            public double sellDailyDiff { get; set; }
+            public double totalDailyDiffPlain { get; set; }
+            public double buyDailyDiffPlain { get; set; }
+            public double sellDailyDiffPlain { get; set; }
         }
     }
 }
