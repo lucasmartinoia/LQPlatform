9f5f95dcc333d3c32afeaaa63e7720a0c73616a4
lucasmartinoia <lucas.martinoia@gmail.com>
2020-03-03 22:12:05 -0300

    Update getAccountPositionsDetailsResponse.cs

--- LQPrimaryAPI/RestAPI/Models/getAccountPositionsDetailsResponse.cs
+++ LQPrimaryAPI/RestAPI/Models/getAccountPositionsDetailsResponse.cs
@@ -8,65 +8,6 @@ namespace LatamQuants.PrimaryAPI.Models
 {
     public static class getAccountPositionsDetailsResponse
     {
-        public class DetailedDailyDiff
-        {
-            public double buyPricePPPDiff { get; set; }
-            public int sellPricePPPDiff { get; set; }
-            public double totalDailyDiff { get; set; }
-            public double buyDailyDiff { get; set; }
-            public double sellDailyDiff { get; set; }
-            public double totalDailyDiffPlain { get; set; }
-            public double buyDailyDiffPlain { get; set; }
-            public double sellDailyDiffPlain { get; set; }
-        }
-
-        public class DetailedPosition2
-        {
-            public string symbolReference { get; set; }
-            public object settlType { get; set; }
-            public string contractType { get; set; }
-            public double priceConversionFactor { get; set; }
-            public double contractSize { get; set; }
-            public double marketPrice { get; set; }
-            public string currency { get; set; }
-            public double exchangeRate { get; set; }
-            public int totalInitialSize { get; set; }
-            public int buyInitialSize { get; set; }
-            public int sellInitialSize { get; set; }
-            public int buyInitialPrice { get; set; }
-            public int sellInitialPrice { get; set; }
-            public double totalFilledSize { get; set; }
-            public double buyFilledSize { get; set; }
-            public int sellFilledSize { get; set; }
-            public double buyFilledPrice { get; set; }
-            public int sellFilledPrice { get; set; }
-            public double totalCurrentSize { get; set; }
-            public double buyCurrentSize { get; set; }
-            public int sellCurrentSize { get; set; }
-            public DetailedDailyDiff detailedDailyDiff { get; set; }
-            public object marketValue { get; set; }
-        }
-
-        //TODO: VER COMO RESOLVER CUANDO HAGA PRUEBAS
-        public class DLR122021
-        {
-            public List<DetailedPosition2> detailedPositions { get; set; }
-            public object instrumentMarketValue { get; set; }
-            public int instrumentInitialSize { get; set; }
-            public double instrumentFilledSize { get; set; }
-            public double instrumentCurrentSize { get; set; }
-        }
-
-        public class FUTURE
-        {
-            public DLR122021 DLR122021 { get; set; }
-        }
-
-        public class Report
-        {
-            public FUTURE FUTURE { get; set; }
-        }
-
         public class DetailedPosition
         {
             public string account { get; set; }
@@ -94,22 +35,56 @@ namespace LatamQuants.PrimaryAPI.Models
                 JObject oJson = JObject.Parse(sJson);
 
                 // get JSON result objects into a list
-                IList<Position> colPositions = oJson["detailedPosition"]["report"].Children().ToList();
+                //IList<Position> colPositions = oJson["detailedPosition"]["report"].Children().ToList();
 
-                // serialize JSON results into .NET objects
-                IList<SearchResult> searchResults = new List<SearchResult>();
-                foreach (JToken result in results)
+                //// serialize JSON results into .NET objects
+                //IList<SearchResult> searchResults = new List<SearchResult>();
+                //foreach (JToken result in results)
+                //{
+                //    // JToken.ToObject is a helper method that uses JsonSerializer internally
+                //    SearchResult searchResult = result.ToObject<SearchResult>();
+                //    searchResults.Add(searchResult);
+                //}
+
+                oReturn.colPositions = new List<Position>();
+                IEnumerable<JToken> aux = oJson.SelectTokens("$..detailedPositions").Children();
+
+                string sSymbolReference = "";
+                Position oPosition = null;
+
+                foreach(var oItem in aux)
                 {
-                    // JToken.ToObject is a helper method that uses JsonSerializer internally
-                    SearchResult searchResult = result.ToObject<SearchResult>();
-                    searchResults.Add(searchResult);
+                    PositionDetails oPosDetails = oItem.ToObject<PositionDetails>();
+
+                    if(oPosDetails.symbolReference!=sSymbolReference)
+                    {
+                        sSymbolReference = oPosDetails.symbolReference;
+
+                        // Save current Position
+                        if (oPosition != null)
+                            oReturn.colPositions.Add(oPosition);
+
+                        // Create first child information of the parent
+                        oPosition = new Position();
+                        oPosition = oItem.ToObject<Position>();
+                        // Get parent information for the instrument
+                        Position oPosition2 = oItem.Parent.Parent.Parent.ToObject<Position>();
+                        oPosition.instrumentCurrentSize = oPosition2.instrumentCurrentSize;
+                        oPosition.instrumentFilledSize = oPosition2.instrumentFilledSize;
+                        oPosition.instrumentInitialSize = oPosition2.instrumentInitialSize;
+                        oPosition.colPositionDetails = new List<PositionDetails>();
+                    }
+                    // Add first child
+                    oPosition.colPositionDetails.Add(oPosDetails);
                 }
+
+                // Save last Position
+                if (oPosition != null)
+                    oReturn.colPositions.Add(oPosition);
 
                 return oReturn;
             }
         }
-
-
 
         public class Position
         {
@@ -130,6 +105,7 @@ namespace LatamQuants.PrimaryAPI.Models
 
         public class PositionDetails
         {
+            public string symbolReference { get; set; }
             public double totalInitialSize { get; set; }
             public double buyInitialSize { get; set; }
             public double sellInitialSize { get; set; }
